87afd77316cefee982931b68cbf9fbef2adb74a6
sitlong <josephsitlong2020@gmail.com>
2021-09-02 12:45:36 +0100

    adding starting project

---
+++ index.js
@@ -0,0 +1,12 @@
+function Function(){
+    document.getElementById("dope").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("dop").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("do").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("don").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("doe").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("de").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("den").innerHTML="Website Thank you for visiting my you are now you have been hack just kidding if you want to become a villan send a message to cybergod549@gmail.com";
+    document.getElementById("button").innerHTML="BYE SUCKER";
+    
+}
+// window.alert("Welcome To My Evil Website")
